b63944f724d30eba1b752e85f6485cc60ba1114c
Минаков Данил Дмитриевич <snomty@yandex.ru>
2025-06-14 16:00:07 +0300

    Fixed bugs

--- common/dataset_combiner.py
+++ common/dataset_combiner.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 
-df_minacov = pd.read_csv("data/df_train_for_cv_minacov.csv")
-df_baryshev = pd.read_csv("data/df_train_for_cv_baryshev.csv")
-df_almetov = pd.read_csv("data/df_train_for_cv_almetov.csv")
+df_minacov = pd.read_csv("../data/df_train_for_cv_minacov.csv")
+df_baryshev = pd.read_csv("../data/df_train_for_cv_baryshev.csv")
+df_almetov = pd.read_csv("../data/df_train_for_cv_almetov.csv")
 
 df = pd.concat([df_almetov, df_baryshev, df_minacov])
 print(df.groupby('label').size())
+print(f"\ntotal tagged photo:\t{df.shape[0]}")
+print(f"total useful tagged photo:\t{df[df['label'] != 0].shape[0]}")
+
+df = df[df['label'] != 0]
+df.to_csv("../data/df_train_for_cv.csv", index=False)
